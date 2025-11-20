3979f01a5d74a2de891d300b1a68153f3433d1c3
Anthony L. Upchurch <156884097+upchurcha400@users.noreply.github.com>
2025-11-20 07:17:06 -0600

    Removed zip added files

---
+++ ACSharp/ACS_aupchurchP1B/ACS_aupchurchP1B/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace ACS_aupchurchP1B
+{
+    partial class frmTitle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTitle));
+            System.Windows.Forms.Label titleLabel;
+            System.Windows.Forms.Label year_PublishedLabel;
+            System.Windows.Forms.Label iSBNLabel;
+            System.Windows.Forms.Label pubIDLabel;
+            this.itse2353pbeDataSet = new ACS_aupchurchP1B.itse2353pbeDataSet();
+            this.titlesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.titlesTableAdapter = new ACS_aupchurchP1B.itse2353pbeDataSetTableAdapters.TitlesTableAdapter();
+            this.tableAdapterManager = new ACS_aupchurchP1B.itse2353pbeDataSetTableAdapters.TableAdapterManager();
+            this.titlesBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.titlesBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.year_PublishedTextBox = new System.Windows.Forms.TextBox();
+            this.iSBNTextBox = new System.Windows.Forms.TextBox();
+            this.pubIDTextBox = new System.Windows.Forms.TextBox();
+            titleLabel = new System.Windows.Forms.Label();
+            year_PublishedLabel = new System.Windows.Forms.Label();
+            iSBNLabel = new System.Windows.Forms.Label();
+            pubIDLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.itse2353pbeDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.titlesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.titlesBindingNavigator)).BeginInit();
+            this.titlesBindingNavigator.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // itse2353pbeDataSet
+            // 
+            this.itse2353pbeDataSet.DataSetName = "itse2353pbeDataSet";
+            this.itse2353pbeDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // titlesBindingSource
+            // 
+            this.titlesBindingSource.DataMember = "Titles";
+            this.titlesBindingSource.DataSource = this.itse2353pbeDataSet;
+            // 
+            // titlesTableAdapter
+            // 
+            this.titlesTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.TitlesTableAdapter = this.titlesTableAdapter;
+            this.tableAdapterManager.UpdateOrder = ACS_aupchurchP1B.itse2353pbeDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // titlesBindingNavigator
+            // 
+            this.titlesBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.titlesBindingNavigator.BindingSource = this.titlesBindingSource;
+            this.titlesBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.titlesBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.titlesBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.titlesBindingNavigatorSaveItem});
+            this.titlesBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.titlesBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.titlesBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.titlesBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.titlesBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.titlesBindingNavigator.Name = "titlesBindingNavigator";
+            this.titlesBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.titlesBindingNavigator.Size = new System.Drawing.Size(426, 25);
+            this.titlesBindingNavigator.TabIndex = 0;
+            this.titlesBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Move first";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Position";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 15);
+            this.bindingNavigatorCountItem.Text = "of {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveNextItem.Text = "Move next";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveLastItem.Text = "Move last";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Add new";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorDeleteItem.Text = "Delete";
+            // 
+            // titlesBindingNavigatorSaveItem
+            // 
+            this.titlesBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.titlesBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("titlesBindingNavigatorSaveItem.Image")));
+            this.titlesBindingNavigatorSaveItem.Name = "titlesBindingNavigatorSaveItem";
+            this.titlesBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 23);
+            this.titlesBindingNavigatorSaveItem.Text = "Save Data";
+            this.titlesBindingNavigatorSaveItem.Click += new System.EventHandler(this.titlesBindingNavigatorSaveItem_Click);
+            // 
+            // titleLabel
+            // 
+            titleLabel.AutoSize = true;
+            titleLabel.Location = new System.Drawing.Point(104, 77);
+            titleLabel.Name = "titleLabel";
+            titleLabel.Size = new System.Drawing.Size(30, 13);
+            titleLabel.TabIndex = 1;
+            titleLabel.Text = "Title:";
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.titlesBindingSource, "Title", true));
+            this.titleTextBox.Location = new System.Drawing.Point(191, 74);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(207, 20);
+            this.titleTextBox.TabIndex = 2;
+            // 
+            // year_PublishedLabel
+            // 
+            year_PublishedLabel.AutoSize = true;
+            year_PublishedLabel.Location = new System.Drawing.Point(104, 103);
+            year_PublishedLabel.Name = "year_PublishedLabel";
+            year_PublishedLabel.Size = new System.Drawing.Size(81, 13);
+            year_PublishedLabel.TabIndex = 3;
+            year_PublishedLabel.Text = "Year Published:";
+            // 
+            // year_PublishedTextBox
+            // 
+            this.year_PublishedTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.titlesBindingSource, "Year_Published", true));
+            this.year_PublishedTextBox.Location = new System.Drawing.Point(191, 100);
+            this.year_PublishedTextBox.Name = "year_PublishedTextBox";
+            this.year_PublishedTextBox.Size = new System.Drawing.Size(207, 20);
+            this.year_PublishedTextBox.TabIndex = 4;
+            // 
+            // iSBNLabel
+            // 
+            iSBNLabel.AutoSize = true;
+            iSBNLabel.Location = new System.Drawing.Point(104, 129);
+            iSBNLabel.Name = "iSBNLabel";
+            iSBNLabel.Size = new System.Drawing.Size(35, 13);
+            iSBNLabel.TabIndex = 5;
+            iSBNLabel.Text = "ISBN:";
+            // 
+            // iSBNTextBox
+            // 
+            this.iSBNTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.titlesBindingSource, "ISBN", true));
+            this.iSBNTextBox.Location = new System.Drawing.Point(191, 126);
+            this.iSBNTextBox.Name = "iSBNTextBox";
+            this.iSBNTextBox.Size = new System.Drawing.Size(207, 20);
+            this.iSBNTextBox.TabIndex = 6;
+            // 
+            // pubIDLabel
+            // 
+            pubIDLabel.AutoSize = true;
+            pubIDLabel.Location = new System.Drawing.Point(104, 155);
+            pubIDLabel.Name = "pubIDLabel";
+            pubIDLabel.Size = new System.Drawing.Size(43, 13);
+            pubIDLabel.TabIndex = 7;
+            pubIDLabel.Text = "Pub ID:";
+            // 
+            // pubIDTextBox
+            // 
+            this.pubIDTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.titlesBindingSource, "PubID", true));
+            this.pubIDTextBox.Location = new System.Drawing.Point(191, 152);
+            this.pubIDTextBox.Name = "pubIDTextBox";
+            this.pubIDTextBox.Size = new System.Drawing.Size(207, 20);
+            this.pubIDTextBox.TabIndex = 8;
+            // 
+            // frmTitle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(426, 208);
+            this.Controls.Add(titleLabel);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(year_PublishedLabel);
+            this.Controls.Add(this.year_PublishedTextBox);
+            this.Controls.Add(iSBNLabel);
+            this.Controls.Add(this.iSBNTextBox);
+            this.Controls.Add(pubIDLabel);
+            this.Controls.Add(this.pubIDTextBox);
+            this.Controls.Add(this.titlesBindingNavigator);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmTitle";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Titles Table With Wizard";
+            this.Load += new System.EventHandler(this.frmTitle_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.itse2353pbeDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.titlesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.titlesBindingNavigator)).EndInit();
+            this.titlesBindingNavigator.ResumeLayout(false);
+            this.titlesBindingNavigator.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private itse2353pbeDataSet itse2353pbeDataSet;
+        private System.Windows.Forms.BindingSource titlesBindingSource;
+        private itse2353pbeDataSetTableAdapters.TitlesTableAdapter titlesTableAdapter;
+        private itse2353pbeDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator titlesBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton titlesBindingNavigatorSaveItem;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox year_PublishedTextBox;
+        private System.Windows.Forms.TextBox iSBNTextBox;
+        private System.Windows.Forms.TextBox pubIDTextBox;
+    }
+}
+
---
+++ ACSharp/ACS_aupchurchP1B/ACS_aupchurchP1B/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ACS_aupchurchP1B
+{
+    public partial class frmTitle : Form
+    {
+        public frmTitle()
+        {
+            InitializeComponent();
+        }
+
+        private void titlesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.titlesBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.itse2353pbeDataSet);
+
+        }
+
+        private void frmTitle_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'itse2353pbeDataSet.Titles' table. You can move, or remove it, as needed.
+            this.titlesTableAdapter.Fill(this.itse2353pbeDataSet.Titles);
+
+        }
+    }
+}
